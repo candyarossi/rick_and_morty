11b41228c5e99f202b3423aae01a566ced0a60f7
Candela Yarossi <candyarossi@gmail.com>
2023-01-30 00:59:59 -0300

    Intro y Estilos

---
+++ src/components/Card.jsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+const DivCard = styled.div`
+  display: inline-block;
+  background-color: darkslateblue;
+  border-radius: 10px;
+  color: white;
+  overflow: hidden;
+  margin: 25px 0px 25px 0px;
+`;
+
+const Button = styled.button`
+  position: relative;
+  right: -120px;
+  top: 10px;
+  background-color: pink;
+  color: purple;
+  border: 0px;
+  width: 30px;
+  height: 30px;
+  border-radius: 5px;
+  font-weight: bold;
+  font-size: 15px;
+`;
+
+const styleSpecie = {
+  display: 'inline-block',
+  fontSize: '16px',
+  color: 'violet',
+  marginRight: '15px',
+  marginTop: '-10px',
+}
+
+const styleGender = {
+  display: 'inline-block',
+  fontSize: '16px',
+  color: 'deeppink',
+  marginTop: '-10px',
+}
+
+// const styleName = {
+//   position: 'relative',
+//   bottom: '-280px',
+//   backgroundColor: 'rgb(0,0,0,0.5)',
+//   padding: '5px',
+//   display: 'inline-block',
+// }
+
+export default function Card(props) {
+  return (
+    <DivCard>
+      <Button onClick={props.onClose}>X</Button>
+      <h2>{props.name}</h2>
+      <h2 style={styleSpecie}>{props.species}</h2>
+      <h2 style={styleGender}>{props.gender}</h2>
+      {/* <h2 style={styleName}>{props.name}</h2> */}
+      <img style={{display: 'block'}} src={props.image} alt="" /> 
+    </DivCard>
+  );
+}
