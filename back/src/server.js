198b46e58af5bd6decc44fb6903daa8893ba89a2
Candela Yarossi <candyarossi@gmail.com>
2023-02-28 13:57:15 -0300

    Update Server

--- back/src/server.js
+++ back/src/server.js
@@ -6,9 +6,10 @@ http
     res.setHeader("Access-Control-Allow-Origin", "*");
     if (req.url.includes("rickandmorty/")) {
       let idCharacter = req.url.split("/").pop();
-      let character = characters.find((e) => e.id === Number(idCharacter));
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify(character));
+      let character = characters.find((e) => e.id == idCharacter);
+      res
+        .writeHead(200, { "Content-Type": "application/json" })
+        .end(JSON.stringify(character));
     }
   })
   .listen(3001, "localhost");
